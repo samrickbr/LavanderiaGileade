7adb4c20bff029953907ae1eaeacf78777510800
Samrick <ricrdocardoso@gmail.com>
2021-02-17 12:24:01 -0300

    Atualizada a tela de cadastro de clientes.xml, função salvar e alterar no Firebase, campos para mensalista.

---
+++ app/src/main/java/com/example/lavanderiagileade/Cliente.java
@@ -0,0 +1,112 @@
+package com.example.lavanderiagileade;
+
+public class Cliente extends clientes {
+    String cpf;
+    String nome;
+    String endereco;
+    String telefone;
+    String email;
+    String sigla;
+    Boolean mensalista;
+    Integer vencimento;
+    Double valor;
+
+    public Cliente(String nome, String endereco, String telefone, String email, String sigla,
+                   Boolean mensalista) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.email = email;
+        this.sigla = sigla;
+        this.mensalista = mensalista;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public Boolean getMensalista() {
+        return mensalista;
+    }
+
+    public void setMensalista(Boolean mensalista) {
+        this.mensalista = mensalista;
+    }
+
+    public Integer getVencimento() {
+        return vencimento;
+    }
+
+    public void setVencimento(Integer vencimento) {
+        this.vencimento = vencimento;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "cpf='" + cpf + '\'' +
+                ", nome='" + nome + '\'' +
+                ", endereco='" + endereco + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", email='" + email + '\'' +
+                ", sigla='" + sigla + '\'' +
+                ", mensalista=" + mensalista +
+                ", vencimento=" + vencimento +
+                ", valor=" + valor +
+                '}';
+    }
+}
